b4b2c008929f7c06faea75204ed9268560a00ff4
Kanban Developer <dev@kanban.local>
2026-04-29 13:23:35 +0500

    Fix font FOUT on splash: preload NexaRustSlab and use font-display block

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,7 @@ export default function RootLayout({
   return (
     <html lang="en" style={{ overflowX: "hidden" }}>
       <head>
+        <link rel="preload" href="/fonts/NexaRustSlab-Black3.ttf" as="font" type="font/truetype" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body className={`${inter.className} overflow-x-hidden`}>
